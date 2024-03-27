f91eda9dc47276808b0c7fb65dc58a5fb3e67770
adelakaca <adelakaca47@gmail.com>
2024-03-27 21:34:50 +0100

    update java version in pom.xml

--- src/main/java/com/tutorials/qa/utils/Utilities.java
+++ src/main/java/com/tutorials/qa/utils/Utilities.java
@@ -4,8 +4,8 @@ import java.util.Date;
 
 public class Utilities {
 	
-	public static final int IMPLICIT_WAIT_TIME=10;
-	public static final int PAGE_LOAD_TIME=5;
+	public static final int IMPLICIT_WAIT_TIME=20;
+	public static final int PAGE_LOAD_TIME=10;
 	
 	public static String generateEmailWithTimeStamp() {
 		
